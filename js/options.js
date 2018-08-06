ad9d424ea5999c6b38cc5a38eb6fe21c85361a4b
thesceptic <thesceptic@126.com>
2018-08-06 16:32:03 +0800

    add css decoration

--- js/options.js
+++ js/options.js
@@ -1,45 +1,49 @@
-document.addEventListener('DOMContentLoaded', function() {
-   // your code here
-   	setTimeout(getPrice, 40)
-   	setTimeout(getGas,   40)
-}, false);
+document.addEventListener(
+  "DOMContentLoaded",
+  function() {
+    // your code here
+    setTimeout(getPrice, 40);
+    setTimeout(getGas, 40);
+  },
+  false
+);
 function sleep(ms) {
   // return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+function httpGet(theUrl) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", theUrl, false); // false for synchronous request
+  xmlHttp.send(null);
+  return xmlHttp.responseText;
 }
 
 function getPrice() {
-	var text = document.getElementById("price");
-	let resp = JSON.parse(httpGet("https://api.coinmarketcap.com/v2/ticker/1027/"))
-	text.innerHTML = resp.data.quotes.USD.price;
+  var text = document.getElementById("price");
+  let resp = JSON.parse(
+    httpGet("https://api.coinmarketcap.com/v2/ticker/1027/")
+  );
+  var price = resp.data.quotes.USD.price;
+  text.innerHTML = price.toString().substring(0, 6);
 }
 
-
 function getGas() {
-	let resp = JSON.parse(httpGet("https://www.etherchain.org/api/gasPriceOracle"))
-	var text = document.getElementById("standard");
-	text.innerHTML = resp.standard;
-	var text = document.getElementById("safeLow");
-	text.innerHTML = resp.safeLow;
-	var text = document.getElementById("fastest");
-	text.innerHTML = resp.fastest;
+  let resp = JSON.parse(
+    httpGet("https://www.etherchain.org/api/gasPriceOracle")
+  );
+  var text = document.getElementById("standard");
+  text.innerHTML = resp.standard.toString().substring(0, 3);
+  var text = document.getElementById("safeLow");
+  text.innerHTML = resp.safeLow.toString().substring(0, 3);
+  var text = document.getElementById("fastest");
+  text.innerHTML = resp.fastest.toString().substring(0, 3);
 }
 
 document.getElementById("button").onclick = function search() {
-	var input = document.getElementById("address");
-	if(input.value.length == 42){
-		chrome.tabs.create({ url: "https://etherscan.io/address/" + input.value })
-	} else {
-
-		chrome.tabs.create({ url: "https://etherscan.io/tx/" + input.value })
-	}
-		
-
-}
+  var input = document.getElementById("address");
+  if (input.value.length == 42) {
+    chrome.tabs.create({ url: "https://etherscan.io/address/" + input.value });
+  } else {
+    chrome.tabs.create({ url: "https://etherscan.io/tx/" + input.value });
+  }
+};
